d933daa1b53621ed81d6b8d40d9e1ffe2ed75fb3
Jeremy Huang <jeremyhuangofficial@gmail.com>
2025-09-30 14:34:00 -0700

    added submit button in Form.jsx, completed app

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,7 +1,7 @@
 // src/Form.jsx
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
     const [person, setPerson] = useState({
         name: "",
         job: ""
@@ -14,6 +14,13 @@ function Form() {
         if (name === "job")
             setPerson({ name: person["name"], job:value });
         else setPerson ({ name:value, job: person["job"] }); 
+    }
+
+    // calls handleSubmit (passed thru props), and pass Form state through person parameter defined
+    // also resets state to the initial state to clear form after submit
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({ name: "", job: "" });
     }
     
     return (
@@ -34,6 +41,7 @@ function Form() {
                 value={person.job}
                 onChange={handleChange}
             />
+            <input type="button" value="Submit" onClick={submitForm} />
         </form>
     );
 }
